0dfda1d738e61ccd8aa28e3ad4465cbfa725158e
Sam Apter <sam@sapter.com>
2019-07-14 15:18:42 -0400

    initial form design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
+import {
+  Grid,
+  Header,
+  Image,
+  Form,
+  Segment,
+  Dropdown,
+} from "semantic-ui-react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <Grid
+        textAlign="center"
+        verticalAlign="middle"
+        style={{ height: "50vh" }}
+      >
+        <Grid.Column className="column">
+          <Header as="h2" textAlign="center">
+            Weather App
+          </Header>
+          <Form>
+            <Form.Input fluid label="Search by" placeholder="Search by" />
+          </Form>
+        </Grid.Column>
+      </Grid>
     </div>
   );
 }
