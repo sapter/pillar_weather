49f8eefe1f72ae3e2cd3a03bc7dc06cbf241626f
Sam Apter <sam@sapter.com>
2019-07-14 21:52:07 -0400

    working app final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
-import { Header, Form, Button, Divider } from "semantic-ui-react";
-// import axios from "axios";
-// import { geolocated } from "react-geolocated";
+import {
+  Header,
+  Form,
+  Button,
+  Divider,
+  Card,
+  Image,
+  List,
+  Icon,
+} from "semantic-ui-react";
 
 import "./App.css";
 
@@ -12,6 +19,7 @@ class App extends Component {
     lon: "",
     city: "",
     weatherData: {},
+    tempCity: "",
   };
 
   componentDidMount() {
@@ -21,21 +29,48 @@ class App extends Component {
     }, 200);
   }
 
-  handleSearchOptionChange = (e, { value }) => {
+  searchOptionChangeHandler = (e, { value }) => {
     this.setState({ searchType: value });
+  };
+
+  cityInputChangeHandler = e => {
+    this.setState({ tempCity: e.target.value });
+  };
+
+  latInputChangeHandler = e => {
+    this.setState({ lat: e.target.value });
+  };
+
+  lonInputChangeHandler = e => {
+    this.setState({ lon: e.target.value });
+  };
+
+  cityButtonClickHandler = () => {
+    setTimeout(() => {
+      this.setState({ city: this.state.tempCity });
+    }, 100);
+    setTimeout(() => {
+      this.fetchWeather();
+    }, 200);
+  };
+
+  coordinatesButtonClickHandler = () => {
+    const { lat, lon } = this.state;
+    setTimeout(() => {
+      this.fetchWeather(lat, lon);
+    }, 5000);
   };
 
   getPosition = () => {
     navigator.geolocation.getCurrentPosition(position => {
       const { latitude, longitude } = position.coords;
       this.setState({ lat: latitude, lon: longitude });
-      console.dir(this.state);
     });
   };
 
   fetchWeather = (lat, lon) => {
     const { searchType, city } = this.state;
-    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=7035ab5008fa909641b7b9e36f9c58de`;
+    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&APPID=7035ab5008fa909641b7b9e36f9c58de`;
     if (searchType[1] === "i") {
       url = `http://api.openweathermap.org/data/2.5/weather?q=${city},us&units=imperial&APPID=7035ab5008fa909641b7b9e36f9c58de`;
     }
@@ -43,8 +78,10 @@ class App extends Component {
       .then(response => response.json())
       .then(data => {
         this.setState({ weatherData: data });
-        console.dir(this.state);
       });
+    setTimeout(() => {
+      this.setState({ city: this.state.weatherData.name });
+    }, 200);
   };
 
   render() {
@@ -58,27 +95,105 @@ class App extends Component {
       },
     ];
 
-    const { searchType } = this.state;
+    const { searchType, city } = this.state;
     let formFields = null;
     if (searchType[1] === "i") {
       formFields = (
-        <Form>
-          <Form.Input fluid label="City name" placeholder="Enter City name" />
-        </Form>
+        <div>
+          <Form>
+            <Form.Input
+              fluid
+              label="City name"
+              placeholder="Enter City name"
+              onChange={this.cityInputChangeHandler}
+            />
+          </Form>
+          <Divider />
+          <Button
+            inverted
+            color="linkedin"
+            fluid
+            onClick={this.cityButtonClickHandler}
+          >
+            Let's Go
+          </Button>
+        </div>
       );
     } else if (searchType[0] === "L") {
       formFields = (
-        <Form>
-          <Form.Input fluid label="Lattitude" placeholder="0 °" />
-          <Form.Input fluid label="Longitude" placeholder="0 °" />
-        </Form>
+        <div>
+          <Form>
+            <Form.Input
+              fluid
+              label="Lattitude"
+              placeholder="0 °"
+              onChange={this.latInputChangeHandler}
+            />
+            <Form.Input
+              fluid
+              label="Longitude"
+              placeholder="0 °"
+              onChange={this.lonInputChangeHandler}
+            />
+          </Form>
+          <Button
+            inverted
+            color="linkedin"
+            fluid
+            onClick={this.coordinatesButtonClickHandler}
+          >
+            Let's Go
+          </Button>
+        </div>
+      );
+    }
+
+    let weatherCard = null;
+    if (city.length) {
+      const { main: weatherStatus, icon } = this.state.weatherData.weather[0];
+      const { main } = this.state.weatherData;
+      const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+      weatherCard = (
+        <Card centered>
+          <Card.Content>
+            <Image floated="right" size="mini" src={iconURL} />
+            <Card.Header>{city}</Card.Header>
+            <Card.Meta>{weatherStatus}</Card.Meta>
+            <Card.Description>
+              <List divided verticalAlign="middle">
+                <List.Item>
+                  <List.Content>
+                    <List.Header>{`Temperature:  ${
+                      main.temp
+                    } °F `}</List.Header>
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    <List.Header>{`Humidity:  ${
+                      main.humidity
+                    } g/M3 `}</List.Header>
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    <List.Header>{`Pressure:  ${
+                      main.pressure
+                    } psi `}</List.Header>
+                  </List.Content>
+                </List.Item>
+              </List>
+            </Card.Description>
+          </Card.Content>
+        </Card>
       );
     }
 
     return (
       <div className="App">
-        <Header as="h2" textAlign="center">
-          Weather App
+        <Header color="violet" as="h2" icon textAlign="center">
+          <Icon name="tree" circular />
+          <Header.Content>Weather App</Header.Content>
         </Header>
         <Form>
           <Form.Select
@@ -86,57 +201,16 @@ class App extends Component {
             label="Search by"
             options={options}
             placeholder="Search by"
-            onChange={this.handleSearchOptionChange}
+            onChange={this.searchOptionChangeHandler}
           />
         </Form>
         {formFields}
         <Divider />
-        <Button
-          inverted
-          color="linkedin"
-          fluid
-          onClick={() => console.log(this.state)}
-        >
-          Let's Go
-        </Button>
+        {weatherCard}
         <Divider />
-        <Button inverted color="twitter" fluid onClick={this.fetchWeather}>
-          Fetch
-        </Button>
-        <Divider />
-        <Button inverted color="google plus" fluid onClick={this.getPosition}>
-          Get Position
-        </Button>
       </div>
     );
   }
 }
-
-// clsss App extends Component {
-//   state = {};
-
-//   const options = [
-//     { key: "c", text: "City", value: "city" },
-//     { key: "l", text: "Longitude and Latitude", value: "longitude and latitude" },
-//   ];
-//   render() {
-
-//     return (
-//       <div className="App">
-//         <Header as="h2" textAlign="center">
-//           Weather App
-//         </Header>
-//         <Form>
-//           <Form.Select
-//             fluid
-//             label="Search by"
-//             options={options}
-//             placeholder="Search by"
-//           />
-//         </Form>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
